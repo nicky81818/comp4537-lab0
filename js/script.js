faad7c0928954ef5c42155d36815ce9675c5c05f
NICOLE_HSU <nicole.d.hsu@gmail.com>
2025-09-05 21:48:09 -0700

    fix logic and clean code

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
 let arrayButtons = [];
 let total;
-const WINDOW_HEIGHT = window.innerHeight
-const WINDOW_WIDTH = window.innerWidth
-console.log(WINDOW_HEIGHT, WINDOW_WIDTH)
-
+let windowWidth;
+let windowHeight;
 
 class Button {
   static counter = 0;
 
   constructor(width, height, top, left, order){
     this.order = order;
+
     this.btn = document.createElement("button");
     this.btn.style.backgroundColor = this.generateRandomColour();
     this.btn.style.width = width;
     this.btn.style.height = height;
-    this.btn.style.position = "absolute"
+    this.btn.style.flexShrink = 0;
+
     this.btn.innerText = order;
     this.btn.disabled = true;
     this.btn.setAttribute("id", order);
+    this.btn.setAttribute("class", "game-btn");
     this.btn.onclick = function () {
-      console.log(Button.counter);
       if (Button.counter !== order) {
         let message = document.createElement("p");
         message.innerText = STRINGS["MESSAGES"]["ERROR"];
@@ -30,35 +30,20 @@ class Button {
       } else {
         document.getElementById(this.id).innerText = this.id;
         Button.counter++;
-        console.log("clicked")
-        if (total === Button.counter) {
+        if (parseInt(total) === Button.counter) {
           let message = document.createElement("p");
           message.innerText = STRINGS["MESSAGES"]["SUCCESS"];
           document.getElementById("message").appendChild(message);
         }
       }
-      // console.log(this)
     };
     document.getElementById("buttons").appendChild(this.btn);
     this.setLocation(top, left);
   }
 
   setLocation (top, left) {
-    // todo fix buttons leaving the screen
-    if (top > WINDOW_HEIGHT) {
-      console.log(this.btn.style.height)
-      this.btn.style.top = top % (WINDOW_HEIGHT - this.btn.style.height);
-      
-    } else {
-      this.btn.style.top = top;
-    }
-    if (left > WINDOW_WIDTH) {
-      this.btn.style.left = left % (WINDOW_WIDTH - this.btn.style.width);
-
-    } else {
-      this.btn.style.left = left;
-    }
-    
+    this.btn.style.top = top;
+    this.btn.style.left = left;
   };
 
   generateRandomColour(){
@@ -74,26 +59,21 @@ class Button {
   reveal() {
     this.btn.innerText = this.order;
   }
-}
-// todo put game related functions in here
-class Game{
-  constructor(){
-    
+
+  static resetCounter() {
+    Button.counter = 0;
   }
-  
+}
+
+class Game{
   static generateGame(){
+    windowWidth = window.innerWidth;
+    windowHeight = window.innerHeight;
     total = document.getElementById("user-input").value;
     if (Game.validateUserInput(total)) {
-  
-      console.log("hi" + total);
-      let x = 100;
-      let y = 100;
       Game.resetGame()
       for (let index = 0; index < total; index++) {
-        arrayButtons.push(new Button("10em", "5em", x + "px", y + "px", index));
-        // x += 200;
-        y += 200;
-        
+        arrayButtons.push(new Button("10em", "5em", "0px","0px", index));
       }
       Game.startGame(total);
     } else {
@@ -104,29 +84,27 @@ class Game{
 
   }
   static validateUserInput(input) {
-    // todo validate 3-7 buttons
     let number = parseInt(input);
-    return number <= 7 && number >= 3;
+    return number >= 3 && number <= 7;
+  }
 
+  static generateRandomPosition(max, border) {
+    return Math.floor(Math.random() * (max - border));
   }
   
   static startGame(total){
     setTimeout(function () {
-      console.log("time to move");
-      let id = setInterval(function() {
-        // console.log(arrayButtons)
+      let intervalId = setInterval(function() {
         arrayButtons.forEach(function(button) {
-          console.log(button.order)
+          button.btn.style.position = "absolute";
           button.setLocation(
-            Math.floor(Math.random() * 1000) + "px",
-            Math.floor(Math.random() * 1000) + "px"
+            Game.generateRandomPosition(windowHeight, button.btn.offsetHeight) + "px",
+            Game.generateRandomPosition(windowWidth, button.btn.offsetWidth) + "px"
           );
-          // console.log(button.style.top)
         })
       }, 2000)
       setTimeout(function () {
-        clearInterval(id);
-        console.log("stopped")
+        clearInterval(intervalId);
         arrayButtons.forEach(function(button) {
           button.activateButton();
         })
@@ -140,11 +118,11 @@ class Game{
     })
     arrayButtons = []
     document.getElementById("message").textContent = ""
+    Button.resetCounter();
   }
 }
   
 class App{
-
   main() {
     this.applyTranslations();
     document.getElementById("go-btn").onclick = () => {
@@ -156,7 +134,6 @@ class App{
     document.getElementById("question").textContent = STRINGS["QUESTION"];
     document.getElementById("go-btn").textContent = STRINGS["GO-BTN"];
   }
-
 }
 
 let app = new App();
